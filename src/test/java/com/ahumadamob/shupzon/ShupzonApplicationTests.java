a41173d97f5ee9f4a1bf8675d721819f6a42136d
Mario Ahumada <ahumadamob@gmail.com>
2025-01-22 22:39:11 -0300

    Spring Initialzr

---
+++ src/test/java/com/ahumadamob/shupzon/ShupzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahumadamob.shupzon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShupzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
